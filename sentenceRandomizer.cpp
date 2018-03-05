384f83f977b98158f735b45c5e8fb9c2085be879
kykorman <kykorman@gmail.com>
2018-03-05 12:01:25 -0600

    Switched to file I/O instead of linux redirection trickery

--- sentenceRandomizer.cpp
+++ sentenceRandomizer.cpp
@@ -3,23 +3,38 @@
 #include <time.h>
 #include <string.h>
 #include <vector>
+#include <fstream>
 using namespace std;
 struct word{
 	char part[50];
 };
-int main(){
+int main(int argc,char **argv){
+
+	if(argc!=2){
+		cout<<"To use this program, use "<<argv[0]<<" <file>\n";
+		exit(1);
+	}
+	ifstream in;
+	in.open(argv[1]);
+
+	if(in==NULL){
+		cout<<"Invalid file specified\n";
+		exit(1);
+	}
 	srand(time(NULL));
+
 	int i=0;
 	int numof=rand()%100;
 	char tmp[50];
+
 	vector<word>msg;
 
-	while(cin>>tmp){
+	in>>tmp;
+	for(int i=0;!in.eof();i++){
 		msg.push_back(word());
 		strcpy(msg[i].part,tmp);
-		i++;
+		in>>tmp;
 	}
-
 	while(numof--){
 		int pos1=rand()%msg.size(),pos2=rand()%msg.size();
 		strcpy(tmp,msg[pos1].part);
